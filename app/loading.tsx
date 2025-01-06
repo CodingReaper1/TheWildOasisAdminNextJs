7f30a2d48e7ecc81d3dede53f4952ea8d1d52675
CodingReaper1 <giorgi.gamertube@gmail.com>
2025-01-06 20:32:37 +0400

    re adding spinner

---
+++ app/loading.tsx
@@ -0,0 +1,11 @@
+import Spinner from "./_components/Spinner";
+
+function loading() {
+  return (
+    <div className="h-[100vh]">
+      <Spinner />;
+    </div>
+  );
+}
+
+export default loading;
